48e01a9377252910710fa36d14d03323a5cb5704
Elif Özker <111710264+elifozker@users.noreply.github.com>
2023-10-24 18:51:17 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete // bir iş sınıfı başka sınıfları newlemez
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        //iş kodları yazılır
+        public List<Product> GetAll() //kurallardan geçtikten sonra return edilir.
+        {
+           return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=> p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice >= min && p.UnitPrice <= max);    
+        }
+    }
+}
