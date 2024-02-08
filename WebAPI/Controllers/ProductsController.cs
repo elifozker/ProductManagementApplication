35f88b9200e396ccfddfc4d413da7758756b78d4
Elif Özker <111710264+elifozker@users.noreply.github.com>
2024-02-08 16:19:39 +0300

    crud operations added

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -18,8 +18,8 @@ namespace WebAPI.Controllers
             _productService = productService;
         }
 
-        [HttpGet]   
-        public IActionResult Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         { 
             var result = _productService.GetAll();
             if (result.Success)
@@ -29,9 +29,19 @@ namespace WebAPI.Controllers
             return BadRequest(result);
 
         }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
 
-        [HttpPost]
-        public IActionResult Post(Product product)
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
         {
             var result = _productService.Add(product);
             if (result.Success)
